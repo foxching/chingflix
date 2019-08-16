3fd2d44a61bac842a1ed93f3a18a5b2f53c2d923
Rechie Lagria <41753498+foxching@users.noreply.github.com>
2019-08-17 00:33:59 +0800

    thunk implementation
    add redux async await

--- src/actions/movieTvActions.js
+++ src/actions/movieTvActions.js
@@ -1,33 +1,43 @@
-import axios from 'axios'
-import { GET_MOVIE_GENRES,  GET_TV_GENRES } from './actionTypes'
+import axios from "axios"
+import {
+  GET_MOVIE_GENRES,
+  GET_TV_GENRES,
+  START_FETCH,
+  END_FETCH
+} from "./actionTypes"
 
 export const getMoviesGenres = () => {
-  return (dispatch) => {
-    axios
-    .get(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=d3de272397bb7105279e2c887f31f0bb&language=en-US"
-    )
-    .then(res => {
-      dispatch({
-        type:GET_MOVIE_GENRES,
-        payload:res.data.genres
-      })
-    });
+  return async dispatch => {
+    try {
+      dispatch({ type: START_FETCH })
+      await delay(1000)
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/genre/movie/list?api_key=d3de272397bb7105279e2c887f31f0bb&language=en-US"
+      )
+      dispatch({ type: GET_MOVIE_GENRES, payload: response.data.genres })
+      dispatch({ type: END_FETCH })
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
-
 export const getTvsGenres = () => {
-  return dispatch => {
-    axios
-      .get(
+  return async dispatch => {
+    try {
+      dispatch({ type: START_FETCH })
+      await delay(1000)
+      const response = await axios.get(
         "https://api.themoviedb.org/3/genre/tv/list?api_key=d3de272397bb7105279e2c887f31f0bb&language=en-US"
       )
-      .then(res => {
-        dispatch({
-          type:GET_TV_GENRES,
-          payload:res.data.genres
-        })
-      });
+      dispatch({ type: GET_TV_GENRES, payload: response.data.genres })
+      dispatch({ type: END_FETCH })
+    } catch (error) {
+      console.log(error)
+    }
   }
+}
+
+const delay = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
