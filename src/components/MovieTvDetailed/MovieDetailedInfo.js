da5aa54e5c20ce7f4d855c910e23ebf3eedc50aa
rechielagria <rechielagria@gmail.com>
2020-01-09 00:39:41 +0800

    still working on movie detailed info UI

--- src/components/MovieTvDetailed/MovieDetailedInfo.js
+++ src/components/MovieTvDetailed/MovieDetailedInfo.js
@@ -1,63 +1,83 @@
-import React from "react";
-import moment from "moment";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { thousandSeparator } from "../../utils/helper";
+import React from 'react';
+import moment from 'moment';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+//import { thousandSeparator } from '../../utils/helper';
 
 const MovieDetailedInfo = ({ movie }) => {
-  let date = moment(`${movie.release_date}`).format("YYYY");
-  console.log(movie);
-  return (
-    <Grid container spacing={4}>
-      <Grid item md={3}>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          width="100%"
-          height="250vh"
-          alt=""
-        />
-      </Grid>
-      <Grid item md={7}>
-        <Typography variant="h4" gutterBottom>
-          {movie.title}
-          <Typography component="span" variant="subtitle2" color="error">
-            ({date})
-          </Typography>
-        </Typography>
+	let date = moment(`${movie.release_date}`).format('YYYY');
+	console.log(movie);
+	return (
+		<React.Fragment>
+			<Grid container spacing={4}>
+				<Grid item md={3}>
+					<img
+						src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+						width="100%"
+						height="250vh"
+						alt=""
+					/>
+				</Grid>
+				<Grid item md={7}>
+					<Typography variant="h4" gutterBottom>
+						{movie.title}
+						<Typography component="span" variant="subtitle2" color="error">
+							({date})
+						</Typography>
+					</Typography>
 
-        <Typography variant="h6">Overview</Typography>
+					<Typography variant="subtitle1">
+						<strong>Run Time:</strong>
+						{movie.runtime}min
+					</Typography>
 
-        <p>{movie.overview}</p>
-        <p>
-          <span style={{ fontWeight: 500 }}>Run Time:</span>{" "}
-          <u>{movie.runtime}mins.</u>{" "}
-        </p>
-        <p>
-          <span style={{ fontWeight: 500 }}>Budget:</span>{" "}
-          <u>${thousandSeparator(movie.budget)}</u>{" "}
-        </p>
-        <p>
-          <span style={{ fontWeight: 500 }}>Revenue:</span>{" "}
-          <u>${thousandSeparator(movie.revenue)}</u>{" "}
-        </p>
-        <p>
-          <span style={{ fontWeight: 500 }}>Genres:</span>
-          {movie.genres.map(genre => (
-            <span> {genre.name} </span>
-          ))}
-        </p>
-        <p>
-          <span style={{ fontWeight: 500 }}>Production Companies:</span>
-          {movie.production_companies.map(company => (
-            <span> {company.name} </span>
-          ))}
-        </p>
-      </Grid>
-      <Grid item md={2}>
-        Rating Here
-      </Grid>
-    </Grid>
-  );
+					<Typography variant="subtitle1" gutterBottom>
+						<strong>Genres:</strong>
+						{movie.genres.map((genre) => <span> {genre.name}, </span>)}
+					</Typography>
+
+					<Typography variant="subtitle1">
+						<strong>Production Companies:</strong>
+						{movie.production_companies.map((company) => (
+							<span style={{ color: 'red' }}> {company.name}, </span>
+						))}
+					</Typography>
+				</Grid>
+				<Grid item md={2}>
+					Rating Here
+				</Grid>
+			</Grid>
+			<Grid container spacing={4}>
+				<Grid item md={6} />
+				<Grid item md={6}>
+					<div>
+						<Button variant="contained" color="secondary" style={{ marginLeft: '30px' }}>
+							Delete
+							<DeleteIcon />
+						</Button>
+						<Button variant="contained" color="secondary" style={{ marginLeft: '30px' }}>
+							Delete
+							<DeleteIcon />
+						</Button>
+						<Button variant="contained" color="secondary" style={{ marginLeft: '30px' }}>
+							Delete
+							<DeleteIcon />
+						</Button>
+					</div>
+				</Grid>
+			</Grid>
+			<Grid container spacing={4}>
+				<Grid item>
+					<Typography variant="h6" color="error">
+						Storyline
+					</Typography>
+					<Typography variant="subtitle1">{movie.overview}</Typography>
+				</Grid>
+			</Grid>
+		</React.Fragment>
+	);
 };
 
 export default MovieDetailedInfo;
--- src/components/Search/SearchDashboard.js
+++ src/components/Search/SearchDashboard.js
@@ -1,78 +1,67 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getSearchMoviesTvs } from "../../actions/movieTvActions";
-import Action from "../Action/Action";
-import ResultsList from "../Results/ResultsList";
-import Pagination from "../Pagination/Pagination";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSearchMoviesTvs } from '../../actions/movieTvActions';
+import Action from '../Action/Action';
+import ResultsList from '../Results/ResultsList';
+import Pagination from '../Pagination/Pagination';
 
 class SearchDashboard extends Component {
-  state = {
-    pageNum: 1,
-    searchTxt: this.props.location.state.detail
-  };
+	state = {
+		pageNum: 1,
+		searchTxt: this.props.location.state.detail
+	};
 
-  UNSAFE_componentWillMount() {
-    this.loadData(
-      this.props.location.state.slug,
-      this.props.location.state.detail,
-      this.state.pageNum
-    );
-  }
+	UNSAFE_componentWillMount() {
+		this.loadData(this.props.location.state.slug, this.props.location.state.detail, this.state.pageNum);
+	}
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (this.props.location.state.detail !== nextProps.location.state.detail) {
-      this.loadData(
-        this.props.location.state.slug,
-        nextProps.location.state.detail,
-        this.state.pageNum
-      );
-      this.setState({ searchTxt: nextProps.location.state.detail });
-    }
-  }
+	UNSAFE_componentWillReceiveProps(nextProps) {
+		if (this.props.location.state.detail !== nextProps.location.state.detail) {
+			this.loadData(this.props.location.state.slug, nextProps.location.state.detail, this.state.pageNum);
+			this.setState({ searchTxt: nextProps.location.state.detail });
+		}
+	}
 
-  loadData = (slug, detail, pageNum) => {
-    if (slug === "search") {
-      this.props.getSearchMoviesTvs(detail, pageNum);
-    }
-  };
+	loadData = (slug, detail, pageNum) => {
+		if (slug === 'search') {
+			this.props.getSearchMoviesTvs(detail, pageNum);
+		}
+	};
 
-  render() {
-    const { queries, loading, page, totalPage, totalResults } = this.props;
-    console.log(queries);
-    return (
-      <div>
-        <main>
-          <Action name="Search Results" loading={loading} />
-          <ResultsList queries={queries} loading={loading} />
-          <Pagination
-            slug={this.props.location.state.slug}
-            searchTxt={this.state.searchTxt}
-            loadData={this.loadData}
-            page={page}
-            totalPage={totalPage}
-            totalResults={totalResults}
-          />
-        </main>
-      </div>
-    );
-  }
+	render() {
+		const { queries, loading, page, totalPage, totalResults } = this.props;
+		console.log(queries);
+		return (
+			<div>
+				<main>
+					<Action name="Search Results" loading={loading} />
+					<ResultsList queries={queries} loading={loading} />
+					<Pagination
+						slug={this.props.location.state.slug}
+						searchTxt={this.state.searchTxt}
+						loadData={this.loadData}
+						page={page}
+						totalPage={totalPage}
+						totalResults={totalResults}
+					/>
+				</main>
+			</div>
+		);
+	}
 }
 
-const mapState = state => {
-  return {
-    queries: state.moviesTvs.queries,
-    loading: state.moviesTvs.loading,
-    page: state.moviesTvs.page,
-    totalPage: state.moviesTvs.totalPage,
-    totalResults: state.moviesTvs.totalResults
-  };
+const mapState = (state) => {
+	return {
+		queries: state.moviesTvs.queries,
+		loading: state.async.loading,
+		page: state.moviesTvs.page,
+		totalPage: state.moviesTvs.totalPage,
+		totalResults: state.moviesTvs.totalResults
+	};
 };
 
 const actions = {
-  getSearchMoviesTvs
+	getSearchMoviesTvs
 };
 
-export default connect(
-  mapState,
-  actions
-)(SearchDashboard);
+export default connect(mapState, actions)(SearchDashboard);
