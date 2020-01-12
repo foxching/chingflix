b21b6a869c77692aed405f228d80a1af14b145ec
foxching <rechielagria@gmail.com>
2020-01-12 08:43:19 +0800

    implement react-modal-video component on trailer button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
 import App from "./App";
+import "../node_modules/react-modal-video/scss/modal-video.scss";
 //import "./styles.css";
 
 const store = configureStore();
